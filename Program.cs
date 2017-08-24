d74694cf66ac1b97c943b21391e017e6b403bd40
Tommi Välkki <m0702367@metropolia.fi>
2017-08-24 12:50:08 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnettest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
